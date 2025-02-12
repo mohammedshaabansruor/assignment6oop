87ce7410622ba9b8bf15548f88c39f002d5a91d4
mohammedshaabansruor <shaabanmohammed490@gmail.com>
2025-02-12 18:23:49 +0200

    p01

--- assignment6oop/Program.cs
+++ assignment6oop/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-           
+            #region P01
+            Point3D point = new Point3D(10);
+            Console.WriteLine(point);
+
+
+
+
+            Point3D[] points =
+            {
+                new Point3D(2,3,4),
+                new Point3D(5,6,7),
+                new Point3D(2,9,10),
+            };
+
+
+
+            Array.Sort(points);
+            foreach (Point3D item in points)
+            {
+                Console.WriteLine(item);
+            }
+
+            #endregion
         }
     }
 }
